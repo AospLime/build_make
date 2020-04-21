2e56a0166f4c192a76d96b530a02f1c600a5ade7
Herobuxx <herobuxx@gmail.com>
2020-04-21 13:16:39 +0700

    build: Changes for AospLime
    Signed-off-by: Herobuxx <herobuxx@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -33,7 +33,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 
 EOF
 
-    __print_magma_functions_help
+    __print_lime_functions_help
 
 cat <<EOF
 
@@ -48,7 +48,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/magma/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/vendor/lime/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -59,8 +59,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/magma/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/magma/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/lime/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/lime/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -142,12 +142,12 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^magma_") ; then
-        MAGMA_BUILD=$(echo -n $1 | sed -e 's/^magma_//g')
+    if (echo -n $1 | grep -q -e "^lime_") ; then
+        LIME_BUILD=$(echo -n $1 | sed -e 's/^lime_//g')
     else
-        MAGMA_BUILD=
+        LIME_BUILD=
     fi
-    export MAGMA_BUILD
+    export LIME_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
@@ -654,16 +654,16 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
-        # if we can't find a product, try to grab it off the MagmaDevices GitHub
+        # if we can't find a product, try to grab it off the LimeDevices GitHub
         T=$(gettop)
         cd $T > /dev/null
-        vendor/magma/build/tools/roomservice.py $product
+        vendor/lime/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/magma/build/tools/roomservice.py $product true
+        vendor/lime/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 
@@ -1632,4 +1632,8 @@ fi
 
 export ANDROID_BUILD_TOP=$(gettop)
 
-. $ANDROID_BUILD_TOP/vendor/magma/build/envsetup.sh
+. $ANDROID_BUILD_TOP/vendor/lime/build/envsetup.sh
+
+echo "======================================="
+echo "      AospLime Build Environment       "
+echo "======================================="
--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -61,6 +61,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "ro.magma.device=$MAGMA_DEVICE"
+echo "ro.lime.device=$LIME_DEVICE"
 
 echo "# end build properties"
